865ea17668b44ae686652798ab35661895a8f3dd
Samay-Nandan <samaynandan@users.noreply.github.com>
2022-08-15 18:19:57 +0530

    Styled Component Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { useSelector } from "react-redux"
 import { useConnectWallet } from "./hooks";
+import { H1 } from "./styles/App.styles";
 
 const App = () => {
-  
-  useConnectWallet()
+
+  useConnectWallet();
+  const wallet = useSelector((state) => state.Wallet);
+
+  if(!wallet.account) return <H1>Connect to Metamask</H1>
 
   return (
-    <>
-    </>
+    <H1>Connected Account: {wallet.account}</H1>
   );
 }
 
